7775913281a06b690aee19f2f1f651a4a59045ae
anthonydinardi <anthonydinardi1@gmail.com>
2023-06-12 21:51:51 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import BillboardCard from '@/components/BillboardCard';
+import Carousel from '@/components/Carousel/Carousel';
+import Image from 'next/image'
+
+export default function Home() {
+  const images: string[] = [
+    '/img/tent-at-night.jpg',
+    '/img/pizza-in-oven.jpg',
+    '/img/trays-of-food.jpg'
+  ];
+
+  return (
+    <main className="flex min-h-screen flex-col items-center space-y-6">
+      <Carousel height={"h-[200px] md:h-[550px]"} imagePaths={images} pauseOnHover={true} />
+
+      <BillboardCard
+        backgroundColor='bg-[#0F4D05]'
+        buttonColor='000000'
+        buttonLink='/'
+        buttonText='Book Now!'
+        textColor='text-[#ffffff]'
+        title='Unleash the Flavor with our Mobile Pizza Kitchen!'
+        subtitle='Perfect for all occasions, our wood-fired pizza catering is ready to elevate your event to new culinary heights.'
+        imagePath='/img/tent-and-trailer.jpg' />
+
+      <BillboardCard
+        backgroundColor='bg-[#580606]'
+        buttonColor='000000'
+        buttonLink='/'
+        buttonText='Book Now!'
+        textColor='text-[#ffffff]'
+        title='900 Degrees. 90 Seconds. Perfection.'
+        subtitle='Experience the blazing speed and intense heat of our wood-fired oven. In just 90 seconds, your pizza emerges perfectly cooked with a charred crust and sensational flavors. Discover pizza perfection at its finest.'
+        imagePath='/img/pizza-in-oven.jpg'
+        imageFirst={true} />
+    </main>
+  )
+}
---
+++ components/Carousel/Carousel.tsx
@@ -0,0 +1,112 @@
+"use client"
+
+import { useEffect, useState } from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+
+interface Props {
+    height?: string;
+    imagePaths: string[];
+    imageDuration?: number;
+    pauseOnHover?: boolean;
+}
+
+function Carousel({ height = "h-[400px]", imagePaths, imageDuration = 7, pauseOnHover = true }: Props) {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
+
+    //goes to previous slide
+    const prevSlide = () => {
+        const lastIndex = imagePaths.length - 1;
+        const shouldResetIndex = currentImageIndex === 0;
+        const index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+        setCurrentImageIndex(index);
+    }
+
+    //goes to next slide
+    const nextSlide = () => {
+        const lastIndex = imagePaths.length - 1;
+        const shouldResetIndex = currentImageIndex === lastIndex;
+        const index = shouldResetIndex ? 0 : currentImageIndex + 1;
+        console.log('index: ', index);
+        setCurrentImageIndex(index);
+    }
+
+    //pauses the carousel when mouse is over
+    const handleMouseEnter = () => {
+        if (pauseOnHover) {
+            setIsPaused(true);
+        }
+    };
+
+    //unpauses the carousel when mouse is not over
+    const handleMouseLeave = () => {
+        if (pauseOnHover) {
+            setIsPaused(false);
+        }
+    };
+
+    //changes the slide every 7 seconds
+    useEffect(() => {
+        let interval: NodeJS.Timeout | null = null;
+
+        const startInterval = () => {
+            interval = setInterval(() => {
+                if (!isPaused) {
+                    nextSlide();
+                }
+            }, imageDuration * 1000);
+        };
+
+        const stopInterval = () => {
+            if (interval) {
+                clearInterval(interval);
+                interval = null;
+            }
+        };
+
+        startInterval();
+
+        return () => {
+            stopInterval();
+        };
+    }, [currentImageIndex, imageDuration, isPaused]);
+
+    return (
+        <div
+            className={`max-w-[1440px] ${height} w-full relative group`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+            <div
+                style={{ backgroundImage: `url(${imagePaths[currentImageIndex]})` }}
+                className='w-full h-full bg-center bg-cover duration-500'
+            ></div>
+
+            {/* left arrow */}
+            <div className='transition-opacity ease-in duration-400 opacity-0 group-hover:opacity-100 absolute top-[50%] -translate-x-0 translate-y-[-50%] 
+            left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                <ChevronLeftIcon onClick={prevSlide} width={30} />
+            </div>
+
+            {/* right arrow */}
+            <div className='transition-opacity ease-in duration-400 opacity-0 group-hover:opacity-100 absolute top-[50%] -translate-x-0 translate-y-[-50%] 
+            right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                <ChevronRightIcon onClick={nextSlide} width={30} />
+            </div>
+
+            {/* dots */}
+            <div className="flex justify-center mt-8 absolute inset-x-0 bottom-6">
+                {imagePaths.map((_, index) => (
+                    <div
+                        onClick={() => setCurrentImageIndex(index)}
+                        key={index}
+                        className={`h-2 w-2 mx-1 rounded-full hover:bg-gray-400 ${currentImageIndex === index ? 'bg-gray-700' : 'bg-gray-300'
+                            }`}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
